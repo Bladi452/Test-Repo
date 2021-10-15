96de13eda1fa35c64c83f3ee24fe9b0b7fddfccb
bladi452 <cleto662@gmail.com>
2021-10-14 22:38:18 -0400

    Inicio

---
+++ src/js/update.js
@@ -0,0 +1,26 @@
+const uaup = require('uaup-js');
+
+const defaultStages = {
+    Checking: "Checking For Updates!", // When Checking For Updates.
+    Found: "Update Found!",  // If an Update is Found.
+    NotFound: "No Update Found.", // If an Update is Not Found.
+    Downloading: "Downloading...", // When Downloading Update.
+    Unzipping: "Installing...", // When Unzipping the Archive into the Application Directory.
+    Cleaning: "Finalizing...", // When Removing Temp Directories and Files (ex: update archive and tmp directory).
+    Launch: "Launching..." // When Launching the Application.
+};
+
+const updateOptions = {
+
+    gitRepo: "Test-Repo", // [Required] Your Repo Name
+    gitUsername: "Bladi452",  // [Required] Your GitHub Username.
+
+    appName: "conforpra", //[Required] The Name of the app archive and the app folder.
+    appExecutableName: "conforpra Setup 1.1.1.exe", //[Required] The Executable of the Application to be Run after updating.
+
+    progressBar: document.getElementById("download"), // {Default is null} [Optional] If Using Electron with a HTML Progressbar, use that element here, otherwise ignore
+    label: document.getElementById("download-label"), // {Default is null} [Optional] If Using Electron, this will be the area where we put status updates using InnerHTML
+    stageTitles: defaultStages, // {Default is defaultStages} [Optional] Sets the Status Title for Each Stage
+};
+
+uaup.Update(updateOptions);
